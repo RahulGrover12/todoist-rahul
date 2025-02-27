a2fb903a55fd2e442a543a799b5f1457b8762f3d
Rahul Grover <groverrahul47@gmail.com>
2025-02-27 12:33:08 +0530

    App file added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Home from "./components/Home";
+import MainLayout from "./layouts/MainLayout";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/inbox" element={<Home />} />
+      <Route path="/:project" element={<Home />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
