bda1a8bd9eb22c775440e329c5b53a4e66099d55
Rahul Grover <groverrahul47@gmail.com>
2025-02-27 21:46:17 +0530

    slider header edited

--- src/components/SliderHeader.jsx
+++ src/components/SliderHeader.jsx
@@ -1,4 +1,4 @@
-import { Layout, Progress } from "antd";
+import { Avatar, Layout, Progress } from "antd";
 import {
   BellOutlined,
   InboxOutlined,
@@ -14,17 +14,17 @@ import { useContext, useState } from "react";
 import AddNewTask from "./AddNewTask";
 import { message } from "antd";
 
-const SliderHeader = ({ isMoveLeft, setIsMoveLeft }) => {
+const SliderHeader = ({ expand, setExpand }) => {
   const navigate = useNavigate();
   const { projects } = useContext(ProjectsContext);
 
   const [isAddTaskClicked, setIsAddTaskClicked] = useState(false);
 
-  const handleAddTaskClicked = (clickedEvent) => {
-    setIsAddTaskClicked(clickedEvent);
+  const handleAddTaskClicked = (e) => {
+    setIsAddTaskClicked(e);
   };
   const handleSliderClick = () => {
-    setIsMoveLeft(!isMoveLeft);
+    setExpand(!expand);
     setIsAddTaskClicked(false);
   };
 
@@ -32,77 +32,89 @@ const SliderHeader = ({ isMoveLeft, setIsMoveLeft }) => {
   const inboxProjectId = inboxProject ? inboxProject.id : null;
 
   return (
-    <Sider
-      width="300px"
-      className={`h-screen bg-[#f9f5f3] relative transition-transform duration-500 ease-in-out transform ${
-        isMoveLeft ? "-translate-x-full" : "translate-x-0"
+    <div
+      className={`relative transition-all duration-500 ease-in-out ${
+        expand ? "bg-white" : ""
       }`}
-      style={{ zIndex: 10, position: "relative" }}
     >
-      <div className="p-2">
-        <div className="flex items-center space-x-4">
-          <Progress
-            size={30}
-            type="circle"
-            percent={50}
-            format={() => (
-              <span className="p-1 pl-[6px] pr-[6px] bg-green-700 rounded-full text-white">
-                R
-              </span>
-            )}
-            strokeColor="#DC4C3E"
-            className="rounded-full mt-2"
-          />
-          <p className="font-bold pt-1.5">RahulGrover</p>
-        </div>
-
-        <div className="absolute right-5 top-2 flex text-gray-500">
-          <BellOutlined
-            className="p-3 text-[18px] cursor-pointer hover:bg-gray-200 rounded-lg"
-            onClick={() => message.success("Notifications on successfully")}
-          />
-          <div
-            onClick={handleSliderClick}
-            className="p-2 cursor-pointer hover:bg-gray-200 hover:text-gray-400 rounded-lg"
-          >
-            <img
-              className="h-[25px] filter grayscale text-gray-500"
-              src={sidebarImage}
-              alt="sidebar"
+      <Sider
+        width="300px"
+        className={`h-screen bg-[#f9f5f3] relative transition-transform duration-500 ease-in-out transform ${
+          expand ? "-translate-x-full" : "translate-x-0"
+        }`}
+        style={{ zIndex: 10, position: "relative" }}
+      >
+        <div className="p-2">
+          <div className="flex items-center space-x-4">
+            <Progress
+              size={40}
+              type="circle"
+              percent={50}
+              strokeColor="#DC4C3E"
+              format={() => (
+                <Avatar
+                  style={{
+                    backgroundColor: "green",
+                    color: "white",
+                    fontSize: 15,
+                  }}
+                >
+                  R
+                </Avatar>
+              )}
+              className="rounded-full mt-2"
             />
+            <p className="font-bold pt-1.5">RahulGrover</p>
+          </div>
+
+          <div className="absolute right-5 top-2 flex text-gray-500">
+            <BellOutlined
+              className="p-3 text-[18px] cursor-pointer hover:bg-gray-200 rounded-lg"
+              onClick={() => message.success("Notifications on successfully")}
+            />
+            <div
+              onClick={handleSliderClick}
+              className="p-2 cursor-pointer hover:bg-gray-200 hover:text-gray-400 rounded-lg"
+            >
+              <img
+                className="h-[25px] filter grayscale text-gray-500"
+                src={sidebarImage}
+                alt="sidebar"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex flex-col gap-1 mt-2">
-        <div
-          onClick={handleAddTaskClicked}
-          className="p-2 pl-4 text-gray-500 font-bold cursor-pointer hover:bg-[#ffefe5] rounded-lg flex items-center"
-        >
-          <PlusCircleFilled className="text-sm text-red-700 bg-gray-100 rounded-full pr-3" />
-          Add Task
+        <div className="flex flex-col gap-1 mt-2">
+          <div
+            onClick={handleAddTaskClicked}
+            className="p-2 pl-4 text-gray-500 font-bold cursor-pointer hover:bg-[#ffefe5] rounded-lg flex items-center"
+          >
+            <PlusCircleFilled className="text-sm text-red-700 bg-gray-100 rounded-full pr-3" />
+            Add Task
+          </div>
+          {isAddTaskClicked && (
+            <AddNewTask
+              values={{
+                handleAddTaskClicked,
+                splitParam: ["Inbox", inboxProjectId],
+                handleAddTask: () => {},
+              }}
+              style={{ padding: "10rem" }}
+            />
+          )}
+          <div
+            onClick={() => navigate("/inbox")}
+            className="p-2 pl-4 text-gray-500 font-bold cursor-pointer hover:bg-[#ffefe5] rounded-lg"
+          >
+            <InboxOutlined className="text-red-600 text-sm pr-3 text-center" />
+            Inbox
+          </div>
+          <Favorites />
+          <MyProject callingFrom={"slider"} />
         </div>
-        {isAddTaskClicked && (
-          <AddNewTask
-            values={{
-              handleAddTaskClicked,
-              splitParam: ["Inbox", inboxProjectId],
-              handleAddTask: () => {},
-            }}
-            style={{ padding: "10rem" }}
-          />
-        )}
-        <div
-          onClick={() => navigate("/inbox")}
-          className="p-2 pl-4 text-gray-500 font-bold cursor-pointer hover:bg-[#ffefe5] rounded-lg"
-        >
-          <InboxOutlined className="text-red-600 text-sm pr-3 text-center" />
-          Inbox
-        </div>
-        <Favorites />
-        <MyProject callingFrom={"slider"} />
-      </div>
-    </Sider>
+      </Sider>
+    </div>
   );
 };
 
