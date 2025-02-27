b78dbb7491ab3a208598261d22469dab79549c6c
Rahul Grover <groverrahul47@gmail.com>
2025-02-27 21:34:19 +0530

    home page edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,14 @@ import MyProject from "./MyProject";
 import Inbox from "./Inbox";
 
 const Home = () => {
-  const [isMoveLeft, setIsMoveLeft] = useState(false);
+  const [expand, setExpand] = useState(false);
   const param = useParams().project;
-  const props = { isMoveLeft, setIsMoveLeft };
+  const props = { expand, setExpand };
 
   return (
     <Layout>
       <SliderHeader {...props} />
-      <Content className="mt-[5px] transition-all duration-500 ease-in-out">
+      <Content className="mt-[5px] transition-all duration-500 ease-in-out bg-white">
         <ContentHeader {...props} />
         {location.pathname === "/inbox" && <Inbox />}
         {!param && location.pathname !== "/inbox" && (
