3f942e4f1f7a1aea95f10d12a090f221c9161767
Rahul Grover <groverrahul47@gmail.com>
2025-02-27 12:30:07 +0530

    todoist api

---
+++ src/api/Api.jsx
@@ -0,0 +1,9 @@
+import { TodoistApi } from "@doist/todoist-api-typescript";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+if (!apiKey) {
+  console.error("Todoist API key is missing!");
+}
+const api = new TodoistApi(apiKey);
+
+export const getApi = () => api;
