6547f6c28d0ea9de0c6f132f122362ebce734679
Rahul Grover <groverrahul47@gmail.com>
2025-02-28 15:48:04 +0530

    tasks list edited

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,18 +1,32 @@
 import { useContext } from "react";
 import Task from "./Task";
+import { ProjectsContext } from "../contexts/ProjectsContext";
 import { TasksContext } from "../contexts/TasksContext";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, showProjectName = false }) => {
+  const { projects } = useContext(ProjectsContext);
   const { handleDeleteTask, handleUpdateTask } = useContext(TasksContext);
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-1">
       {tasks.length > 0 ? (
-        tasks.map((task) => (
-          <Task
-            key={task.id}
-            values={{ task, handleDeleteTask, handleUpdateTask }}
-          />
-        ))
+        tasks.map((task) => {
+          const project = projects.find(
+            (project) => project.id === task.projectId
+          );
+          return (
+            <div
+              key={task.id}
+              className="p-1 border-b border-gray-300 flex items-center"
+            >
+              <Task values={{ task, handleDeleteTask, handleUpdateTask }} />
+              {showProjectName && project && (
+                <span className="text-gray-500 text-xs italic ml-4 text-nowrap">
+                  {project.name}
+                </span>
+              )}
+            </div>
+          );
+        })
       ) : (
         <p className="text-gray-400 px-4">No tasks available</p>
       )}
