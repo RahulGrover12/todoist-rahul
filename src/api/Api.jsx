e9cae6ebd609dc6005965850bce9a4222354677b
Rahul Grover <groverrahul47@gmail.com>
2025-02-28 11:36:48 +0530

    api key added

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -2,6 +2,7 @@ import { TodoistApi } from "@doist/todoist-api-typescript";
 
 const apiKey =
   import.meta.env.VITE_API_KEY || "3f36f5d74d3189ba1997fd1a9a310b20eacb9a2d";
+
 if (!apiKey) {
   console.error("Todoist API key is missing!");
 }
